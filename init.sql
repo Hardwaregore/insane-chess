8a07531ee7f75d166abf584179201581097b4ef2
Hardwaregore <hardwaregore101@gmail.com>
2022-05-21 16:46:11 -0500

    AAAAAAAAHHHHHHHHHHHHH

--- init.sql
+++ init.sql
@@ -6,8 +6,7 @@ CREATE TABLE users (
     `password` VARCHAR(255) NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `banned` BOOL DEFAULT FALSE NOT NULL,
-    `vootos` INT(5) DEFAULT 0 NOT NULL,
-    -- 'vootos' means 'violations of our terms of service',
+    `vootos` INT DEFAULT 0 NOT NULL,
     `level` INT DEFAULT 0 NOT NULL,
     `points` INT DEFAULT 0 NOT NULL,
     `admin` BOOL DEFAULT FALSE NOT NULL
---
+++ insanechess.session.sql
@@ -0,0 +1,6 @@
+USE `insanechess`;
+
+SHOW DATABASES;
+
+
+
