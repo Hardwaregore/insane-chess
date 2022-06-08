c58d8a4c449798de6f5786aacc8627b386ceae05
Hardwaregore <hardwaregore101@gmail.com>
2022-06-07 21:07:36 -0500

    Some extreamly small minor changes to source code

--- Backend/unban.php
+++ Backend/unban.php
@@ -1,18 +1,33 @@
 <?php
     require_once "connect.php";
-    session_start();
-            if (isset($_POST['username'])) {
-                $username = $_POST['username'];
-                $reason = "null";
-                $banned_by = "null";
+    require_once "checkadmin.php";
 
-                $sql = "UPDATE users SET banned = 0, ban_reason = '$reason', banned_by = '$banned_by' WHERE username = '$username';";
-                $result = mysqli_query($conn, $sql);
-                if ($result) {
-                    echo "User Unbanned!";
-                    
-                } else {
-                    echo "Error Unbanning user!";
-                }
-            }
+
+    // checks if there is a username in the url
+    if (isset($_GET['username'])) {
+
+    // Get the username from the URL
+    $username = $_GET['username'];
+    // Unban the user
+    $sql = "UPDATE users SET banned = 0, ban_reason = 'null', banned_by = 'null' WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    // Delete the appeal
+    $sql = "DELETE FROM appeals WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+
+    // print unban success message
+    if ($result) {
+        echo "User unbanned!";
+        
+    } else {
+        echo "Error unbanning user!";
+    }
+
+    // Redirect to the unban page
+    header("Location: unban_user.php");
+} else {
+    //
+}
+
+           
         ?>
--- Backend/unban_user.php
+++ Backend/unban_user.php
@@ -5,7 +5,7 @@
 
 <html>
     <head>
-        <title>Ban a user</title>
+        <title>Unban a user</title>
         <style>
             table, th, td {
                 border:1px solid black;
@@ -21,6 +21,12 @@
         <!-- Puts all ban appeals in a table -->
         <!-- Admins can accept or reject the appeal by clinking on each row of the table -->
         <h1>Unban a user</h1>
+            <form action="search.php" method="post">
+                <!-- search for a banned user -->
+                <input type="text" name="username" placeholder="Username">
+                <button type="submit" value="Search">Search</button>
+            </form>
+        
         <table style="width:100%">
             <tr>
                 <th>Username</th>
@@ -34,7 +40,7 @@
                     echo "<tr>";
                     echo "<td>".$row['username']."</td>";
                     echo "<td>".$row['reason']."</td>";
-                    echo "<td><a href='Backend/acceptappeal.php?id=".$row['id']."'>Accept</a> / <a href='Backend/rejectappeal.php?id=".$row['id']."'>Reject</a></td>";
+                    echo "<td><a href='unban.php?username=".$row['username']."'>Accept</a> / <a href='rejectappeal.php?username=".$row['username']."'>Reject</a></td>";
                     echo "</tr>";
                 }
             ?>
