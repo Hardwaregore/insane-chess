7b40a67986d9165aff3ac64f90f05f66e4daab47
Hardwaregore <hardwaregore101@gmail.com>
2022-06-18 18:23:26 -0600

    BRUH

--- init.sql
+++ init.sql
@@ -8,14 +8,6 @@ CREATE TABLE `users` (
     `banned` BOOL DEFAULT FALSE NOT NULL,
     `ban_reason` VARCHAR(255) DEFAULT NULL,
     `banned_by` VARChAR(255) DEFAULT NULL,
-    `level` INT DEFAULT 0 NOT NULL,
-    `points` INT DEFAULT 0 NOT NULL,
-    `games-played` INT DEFAULT 0 NOT NULL,
-    `games-won` INT DEFAULT 0 NOT NULL,
-    `games-lost` INT DEFAULT 0 NOT NULL,
-    `games-drawn` INT DEFAULT 0 NOT NULL,
-    `busy` BOOL DEFAULT FALSE NOT NULL,
-    `status` DATETIME DEFAULT NULL,
     `admin` BOOL DEFAULT FALSE NOT NULL
 );
 
@@ -28,5 +20,19 @@ CREATE TABLE `appeals` (
     `reject_reason` VARCHAR(255) DEFAULT NULL
 );
 
+CREATE TABLE `player_data` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `games_played` INT NOT NULL,
+    `games_won` INT NOT NULL,
+    `games_lost` INT NOT NULL,
+    `games_drawn` INT NOT NULL,
+    `games_aborted` INT NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `friends` VARCHAR(255) NOT NULL,
+    `achievements` VARCHAR(255) NOT NULL,
+    `points` INT NOT NULL,
+    `level` INT NOT NULL
+);
 
 
