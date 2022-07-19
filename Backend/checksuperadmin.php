4a4094edcc93f2587260c28009f42ef5cd95e161
Hardwaregore <hardwaregore101@gmail.com>
2022-07-18 19:54:46 -0500

    lol

--- Backend/checksuperadmin.php
+++ Backend/checksuperadmin.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 $checksuperadmin = "SELECT adnin FROM users WHERE username = '$_SESSION[username]'";
 $resultofchecksuperadmin = mysqli_query($conn, $checksuperadmin);
 $row = mysqli_fetch_assoc($resultofchecksuperadmin);
